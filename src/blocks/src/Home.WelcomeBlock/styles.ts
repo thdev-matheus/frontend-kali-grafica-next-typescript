8a254edc2ad0ab64034fee9ed68a9300bedc0519
Theus <matheusth.dev@gmail.com>
2023-10-25 19:24:59 -0300

    feat: welcome block mobile query

--- src/blocks/src/Home.WelcomeBlock/styles.ts
+++ src/blocks/src/Home.WelcomeBlock/styles.ts
@@ -1,89 +1,87 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  @media (min-width: 481px) {
-  }
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
-    width: 100%;
-
-    display: flex;
     flex-direction: row;
-    justify-content: center;
-    align-items: center;
 
     padding: 0 10% 0 0;
-  }
-
-  @media (min-width: 1920px) {
   }
 `;
 
 export const BoxLeft = styled.section`
-  @media (min-width: 481px) {
+  width: 100%;
+
+  & > img {
+    width: 85%;
   }
 
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
     width: 50%;
 
     & > img {
       width: 100%;
     }
   }
-
-  @media (min-width: 1920px) {
-  }
 `;
 
 export const BoxRight = styled.section`
-  @media (min-width: 481px) {
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  padding: 2rem;
+
+  & > h1 {
+    margin-bottom: 1rem;
+
+    font-size: 16pt;
+
+    text-align: center;
+
+    color: ${(props) => props.theme.primary};
+  }
+
+  & > p {
+    font-size: 12pt;
+
+    text-align: center;
   }
 
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
     width: 50%;
     height: 100%;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-
     padding: 0 1rem;
+  }
+
+  @media (min-width: 992px) {
+    gap: 1rem;
 
     & > h1 {
       margin-bottom: 2rem;
 
-      font-size: 22pt;
+      font-size: 18pt;
+    }
+  }
 
-      text-align: center;
+  @media (min-width: 1200px) {
+    & > h1 {
+      font-size: 22pt;
     }
 
     & > p {
       font-size: 16pt;
-
-      text-align: center;
     }
-  }
-
-  @media (min-width: 1920px) {
   }
 `;
