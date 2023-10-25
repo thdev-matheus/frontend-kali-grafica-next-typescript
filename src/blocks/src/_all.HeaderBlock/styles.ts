c273923c0d53334feb8556ca474c495d4c108fec
Theus <matheusth.dev@gmail.com>
2023-10-25 17:38:22 -0300

    feat: header block mobile query

--- src/blocks/src/_all.HeaderBlock/styles.ts
+++ src/blocks/src/_all.HeaderBlock/styles.ts
@@ -1,85 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  @media (min-width: 481px) {
-  }
+  width: 100%;
+  height: 4rem;
 
-  @media (min-width: 768px) {
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  @media (min-width: 992px) {
-  }
+  padding: 0.7rem 0;
 
-  @media (min-width: 1200px) {
-    width: 100%;
-    height: 4rem;
+  background-color: ${(props) => props.theme.bgSecondary};
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 0.7rem 0;
-
-    background-color: ${(props) => props.theme.bgSecondary};
-
-    & > img {
-      height: 100%;
-    }
-  }
-
-  @media (min-width: 1920px) {
+  & > img {
+    height: 100%;
   }
 `;
 
 export const BoxColors = styled.div`
-  @media (min-width: 481px) {
+  width: 100%;
+  height: 0.3rem;
+
+  display: flex;
+
+  & > .cyan {
+    height: 100%;
+
+    flex: 1;
+
+    background-color: cyan;
   }
 
-  @media (min-width: 768px) {
+  & > .magenta {
+    height: 100%;
+
+    flex: 1;
+
+    background-color: magenta;
   }
 
-  @media (min-width: 992px) {
+  & > .yellow {
+    height: 100%;
+
+    flex: 1;
+
+    background-color: yellow;
   }
 
-  @media (min-width: 1200px) {
-    width: 100%;
-    height: 0.3rem;
+  & > .black {
+    height: 100%;
 
-    display: flex;
+    flex: 3;
 
-    & > .cyan {
-      height: 100%;
-
-      flex: 1;
-
-      background-color: cyan;
-    }
-
-    & > .magenta {
-      height: 100%;
-
-      flex: 1;
-
-      background-color: magenta;
-    }
-
-    & > .yellow {
-      height: 100%;
-
-      flex: 1;
-
-      background-color: yellow;
-    }
-
-    & > .black {
-      height: 100%;
-
-      flex: 3;
-
-      background-color: #000000;
-    }
-  }
-
-  @media (min-width: 1920px) {
+    background-color: #000000;
   }
 `;
