d04fa578d98ed9097f1c9384bd0fe7b821c7f11e
Theus <matheusth.dev@gmail.com>
2023-10-26 21:54:31 -0300

    feat: call to action social media block mobile query

--- src/blocks/src/Home.CallSocialMediaBlock/styles.ts
+++ src/blocks/src/Home.CallSocialMediaBlock/styles.ts
@@ -1,50 +1,58 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  @media (min-width: 481px) {
-  }
+  width: 100%;
 
-  @media (min-width: 768px) {
-  }
-
-  @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
-    width: 100%;
-
-    display: flex;
-    justify-content: center;
-  }
+  display: flex;
+  justify-content: center;
 `;
 
 export const BoxLeft = styled.section`
-  @media (min-width: 481px) {
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  padding: 2rem 10%;
+
+  text-align: center;
+
+  & > h3 {
+    font-size: 18pt;
+
+    color: ${(props) => props.theme.primary};
   }
 
-  @media (min-width: 768px) {
+  & > p {
+    font-size: 12pt;
+  }
+
+  @media (min-width: 481px) {
+    & > h3 {
+      font-size: 22pt;
+    }
   }
 
   @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
     width: 60%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
 
     padding: 2rem 0 2rem 10%;
 
-    text-align: center;
+    & > h3 {
+      font-size: 18pt;
+    }
 
+    & > p {
+      font-size: 10pt;
+    }
+  }
+
+  @media (min-width: 1200px) {
     & > h3 {
       font-size: 22pt;
-
-      color: ${(props) => props.theme.primary};
     }
 
     & > p {
@@ -54,17 +62,12 @@ export const BoxLeft = styled.section`
 `;
 
 export const BoxRigth = styled.section`
-  @media (min-width: 481px) {
-  }
-
-  @media (min-width: 768px) {
-  }
+  display: none;
 
   @media (min-width: 992px) {
-  }
-
-  @media (min-width: 1200px) {
     width: 40%;
+
+    display: block;
 
     overflow: hidden;
 
